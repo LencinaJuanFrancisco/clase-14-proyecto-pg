ce0abc855844dc4f9a7afdb1a278e7346ee558f7
lencinajuanfrancisco <juanfranciscolencina@gmail.com>
2021-05-04 08:40:56 -0300

    se modifica el puerto en app.js y de agrega el Procfile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path=require('path');
 
 app.use(express.static(path.resolve(__dirname,'./public')));
 
-app.listen(3030,()=>{
+app.listen(process.env.PORT || 3030,()=>{
     console.log('SERVIDOR corriendo en http://localhost:3030/home');
 })
 app.get('/login',(req,res)=>{
